810f6a36eec3ce5be8673c11311bf1dcc506c3f9
yukang <moorekang@gmail.com>
2023-04-21 18:00:09 +0800

    make timeout example simple

--- examples/server-timeout.rs
+++ examples/server-timeout.rs
@@ -3,15 +3,8 @@
 use std::{sync::Arc, time::Duration};
 
 use axum::{error_handling::HandleErrorLayer, http::StatusCode, BoxError};
-use futures_util::{SinkExt, TryStreamExt};
 use jsonrpc_core::{MetaIoHandler, Params};
-use jsonrpc_utils::{
-    axum_utils::jsonrpc_router,
-    pub_sub::{add_pub_sub, PublishMsg},
-    stream::{serve_stream_sink, StreamMsg, StreamServerConfig},
-};
-use tokio::net::TcpListener;
-use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec, LinesCodecError};
+use jsonrpc_utils::{axum_utils::jsonrpc_router, stream::StreamServerConfig};
 use tower::ServiceBuilder;
 
 #[tokio::main]
@@ -33,30 +26,6 @@ async fn main() {
         Ok(sum.into())
     });
 
-    add_pub_sub(
-        &mut rpc,
-        "subscribe",
-        "subscription",
-        "unsubscribe",
-        |params: Params| {
-            let (interval,): (u64,) = params.parse()?;
-            if interval > 0 {
-                Ok(async_stream::stream! {
-                    for i in 0..10 {
-                        tokio::time::sleep(Duration::from_secs(interval)).await;
-                        yield PublishMsg::result(&i);
-                    }
-                    yield PublishMsg::error(&jsonrpc_core::Error {
-                        code: jsonrpc_core::ErrorCode::ServerError(-32000),
-                        message: "ended".into(),
-                        data: None,
-                    });
-                })
-            } else {
-                Err(jsonrpc_core::Error::invalid_params("invalid interval"))
-            }
-        },
-    );
     let rpc = Arc::new(rpc);
     let stream_config = StreamServerConfig::default()
         .with_channel_size(4)
@@ -76,31 +45,9 @@ async fn main() {
             .serve(app.into_make_service())
             .await
             .unwrap();
-    });
-
-    // TCP server with line delimited json codec.
-    //
-    // You can also use other transports (e.g. TLS, unix socket) and codecs
-    // (e.g. netstring, JSON splitter).
-    let listener = TcpListener::bind("0.0.0.0:3001").await.unwrap();
-    let codec = LinesCodec::new_with_max_length(2 * 1024 * 1024);
-    while let Ok((s, _)) = listener.accept().await {
-        let rpc = rpc.clone();
-        let stream_config = stream_config.clone();
-        let codec = codec.clone();
-        tokio::spawn(async move {
-            let (r, w) = s.into_split();
-            let r = FramedRead::new(r, codec.clone()).map_ok(StreamMsg::Str);
-            let w = FramedWrite::new(w, codec).with(|msg| async move {
-                Ok::<_, LinesCodecError>(match msg {
-                    StreamMsg::Str(msg) => msg,
-                    _ => "".into(),
-                })
-            });
-            tokio::pin!(w);
-            drop(serve_stream_sink(&rpc, w, r, stream_config).await);
-        });
-    }
+    })
+    .await
+    .unwrap();
 }
 
 async fn handle_timeout_error(err: BoxError) -> (StatusCode, String) {
